29fe9e9b0292a4c984789a028833d67568fa8a98
Vijon Baraku <vijon.baraku@asseco-see.com>
2022-10-19 14:32:03 +0000

    add code in files types: .java,.js remove code in files types: .java,.js

--- content.java
+++ content.java
@@ -26,6 +26,6 @@ public class Cmecxb {
         System.out.println("yfhdf");
         System.out.println("gnpft");
         System.out.println("gbslw");
-        System.out.println("obsve");
+        System.out.println("cxqid");
     }
 }
