a7b5f8aa4611b4261c03cf64fe771b7c55d40164
Vijon Baraku <vijon.baraku@asseco-see.com>
2022-11-29 08:47:09 +0000

    add code in files types: .xml,.java remove code in files types: .xml,.java

--- content.java
+++ content.java
@@ -18,6 +18,6 @@ public class Cmecxb {
         System.out.println("illej");
         System.out.println("wvsvq");
         System.out.println("bwvba");
-        System.out.println("kebso");
+        System.out.println("bmnse");
     }
 }
